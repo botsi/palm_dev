3ca4081a0061371d3fce0bd4be550abcda01885b
botsi <david@abotsi.com>
2017-04-06 01:11:39 +0200

    hg image webp test

--- js/main.js
+++ js/main.js
@@ -2038,7 +2038,11 @@ var text_load = function() {
 
                 check_webp_feature('lossless', function(result) {
                     console.log(result);
+                    if (!result) {} else {
+                        document.getElementById('hg').style.backgroundImage = 'images/webp/sonderausstellungen/donnergrollen_3.webp';
+                    }
                     page_load();
+
                 });
 
             } else {
