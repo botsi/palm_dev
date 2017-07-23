bf3644b86876af2c705b09dbf3ec512a489b82d7
botsi <david@abotsi.com>
2017-07-23 02:13:39 +0200

    okok

--- scripts/new_e.php
+++ scripts/new_e.php
@@ -10,10 +10,7 @@ if (!$_SESSION["login"] == 1 || !isJSON($str_json)){
 }else{
     $file = '../inhalt.js';
     file_put_contents($file, $str_json);
-<<<<<<< HEAD
-    echo 'ok botsi palm_dev palm_prod 87db6352b33391ae77cf8dc3420057f280ab422a inhalt.js';
-=======
-    echo 'ok botsi palm_dev master c094a8cdaf16c004f883281be1959e8ce9f4d39c inhalt.js';
->>>>>>> origin/palm_prod
+
+    echo 'ok botsi palm_dev palm_prod f592b9ae7225ab1ed83efb480d28d1a8d4e18f49 inhalt.js';
 }
 ?>
