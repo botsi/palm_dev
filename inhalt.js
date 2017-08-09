689cba465ad2bd8398f5945f8de36a26b202c47c
botsi <david@abotsi.com>
2017-08-09 19:34:39 +0200

    Fernes Donnergrollen / david@abotsi.com / 9. Aug 2017 / 19:34:37

--- inhalt.js
+++ inhalt.js
@@ -1,9 +1,11 @@
 {
-	"folders": [{
+	"folders": [
+		{
 			"menu_name": "Über uns",
 			"name": "uberuns",
 			"intro": "Ausstellungsbüro für Kopfreisen",
-			"data": [{
+			"data": [
+				{
 					"comp_name": "profil",
 					"name": "Profil",
 					"search": [
@@ -196,7 +198,8 @@
 			"menu_name": "Sonderausstellungen",
 			"name": "sonderausstellungen",
 			"intro": "Ausstellungen zu Geschichte, Kultur und Gesellschaft.",
-			"data": [{
+			"data": [
+				{
 					"comp_name": "schreibrausch",
 					"name": "Schreibrausch",
 					"search": [
@@ -666,7 +669,7 @@
 							"Kuratoren": [
 								"Andreas Schwab (palma3)",
 								"Ronny Trachsel (palma3)",
-								"Mario Florin"
+								"<a href='http://post.ch' target='_blank'>Mario Florin</a>"
 							],
 							"Gestaltung": [
 								"<a href='http://www.z-a-dire.ch/' target='_blank'>Richard Zeerleder, z-à-dire</a>"
@@ -1488,7 +1491,8 @@
 			"menu_name": "Dauerausstellungen",
 			"name": "dauerausstellungen",
 			"intro": "",
-			"data": [{
+			"data": [
+				{
 					"comp_name": "fondazionemonteverita",
 					"name": "Monte Verità",
 					"search": [
@@ -1861,7 +1865,8 @@
 			"menu_name": "Beratung",
 			"name": "beratung",
 			"intro": "",
-			"data": [{
+			"data": [
+				{
 					"comp_name": "sasso",
 					"name": "Sasso San Gottardo",
 					"search": [
@@ -2026,7 +2031,8 @@
 			"menu_name": "Audio und Video",
 			"name": "audioundvideo",
 			"intro": "",
-			"data": [{
+			"data": [
+				{
 					"columns": "",
 					"comp_name": "maxfrisch",
 					"name": "Auf den Spuren von Max Frisch",
@@ -2195,7 +2201,8 @@
 			"menu_name": "Publikationen",
 			"name": "publikationen",
 			"intro": "",
-			"data": [{
+			"data": [
+				{
 					"name": "Anderberg",
 					"comp_name": "anderberg",
 					"search": [
